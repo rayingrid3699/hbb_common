1208161b68c0e3e454538567ab63548dfc4fd548
rayingrid3699 <rayingrid369@gmail.com>
2025-04-26 11:12:52 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,7 +98,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["b.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.2.180"];
 pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
